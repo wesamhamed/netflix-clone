2a57eac08f25ec3dbbee3bea1e1fb20808e78542
wesam hamed <w.m.h.h2022@gmail.com>
2021-04-24 22:46:33 +0300

    configration of firebase

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 import { seedDatabase } from "../seed.js";
 
 const config = {
-    apiKey: "AIzaSyB12OxCdjTm3_fgy0l9wn2Lc3R7AFRi13Q",
-    authDomain: "netflix-clone-1ae3b.firebaseapp.com",
-    projectId: "netflix-clone-1ae3b",
-    storageBucket: "netflix-clone-1ae3b.appspot.com",
-    messagingSenderId: "719152475534",
-    appId: "1:719152475534:web:cf1f9e14ef8b15de7d08df"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIRBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 const firebase = Firebase.initializeApp(config);
