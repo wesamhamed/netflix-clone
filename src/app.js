a56871769f348929b16e276909f4c1e5d3fd6f3b
wesam hamed <w.m.h.h2022@gmail.com>
2021-04-24 15:20:30 +0300

    setup the routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { JumbotronContainer } from "./containers/jumbotron";
-import { FooterContainer } from "./containers/footer";
-import { FaqsContainer } from './containers/faqs';
+import {BrowserRouter as Router, Route } from "react-router-dom";
+import {Home,Browse,SignIn,SignUp} from "./pages";
+import * as ROUTES from "./constants/routes";
+
 export function App() {
 
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-
-    </>
+    <Router>
+      <Route path={ROUTES.HOME} exact component={Home}/>
+      <Route path={ROUTES.BROWSE} exact component={Browse}/>
+      <Route path={ROUTES.SIGN_IN} exact component={SignIn}/>
+      <Route path={ROUTES.SIGN_UP} exact component={SignUp}/>
+    </Router>
   );
 }
