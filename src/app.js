bb3dfd9b0aea2667dd6a65602094111df28e3840
wesam hamed <w.m.h.h2022@gmail.com>
2021-04-25 18:49:53 +0300

    Add Protected Routes .

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import {BrowserRouter as Router, Route } from "react-router-dom";
-import {Home,Browse,SignIn,SignUp} from "./pages";
+import { BrowserRouter as Router,Switch } from "react-router-dom";
+import { Home, Browse, SignIn, SignUp } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect,ProtectedRoute } from "./helpers/routes";
 
 export function App() {
+  const user =null;
 
   return (
     <Router>
-      <Route path={ROUTES.HOME} exact component={Home}/>
-      <Route path={ROUTES.BROWSE} exact component={Browse}/>
-      <Route path={ROUTES.SIGN_IN} exact component={SignIn}/>
-      <Route path={ROUTES.SIGN_UP} exact component={SignUp}/>
+          <Switch>
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+                    <SignIn/>
+            </IsUserRedirect>
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+                    <SignUp/>
+            </IsUserRedirect>
+            <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+              <Browse/>
+            </ProtectedRoute>
+            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} exact>
+              <Home/>
+            </IsUserRedirect>
+          </Switch>
     </Router>
   );
 }
