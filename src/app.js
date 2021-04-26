b10d0b3047bfa013bd03d1e6fca8df24b25ce699
wesam hamed <w.m.h.h2022@gmail.com>
2021-04-26 03:05:16 +0300

    Add use-content hook

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router,Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Home, Browse, SignIn, SignUp } from "./pages";
 import * as ROUTES from "./constants/routes";
-import { IsUserRedirect,ProtectedRoute } from "./helpers/routes";
-
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 export function App() {
-  const user =null;
+  const {user} =useAuthListener();
 
   return (
     <Router>
-          <Switch>
-            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
-                    <SignIn/>
-            </IsUserRedirect>
-            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
-                    <SignUp/>
-            </IsUserRedirect>
-            <ProtectedRoute user={user} path={ROUTES.BROWSE}>
-              <Browse/>
-            </ProtectedRoute>
-            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} exact>
-              <Home/>
-            </IsUserRedirect>
-          </Switch>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} exact>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
